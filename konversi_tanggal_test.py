e599f681c8b3a12a2e68d51dcf4156ddf1a828a8
Adh <adh.isl.almrjl@gmail.com>
2021-04-04 20:24:24 +0800

    Updated testing

--- konversi_tanggal_test.py
+++ konversi_tanggal_test.py
@@ -1,19 +1,54 @@
 from HijriahMasehiKonverter import HijriahKeMasehi, MasehiKeHijriah
+import unittest
 
+class Test(unittest.TestCase):
 
-test = HijriahKeMasehi(1421, 1, 1)
-test2 = HijriahKeMasehi(1444, 10, 12)
-test3 = HijriahKeMasehi(1342, 2, 29)
-test4 = MasehiKeHijriah(1991, 8, 13)
-test5 = MasehiKeHijriah(2000, 28, 1)
-test6 = MasehiKeHijriah(2020, 1, 15)
-                    
-print("Hijriah ke Masehi")
-print(test.convert_to_gregorian())
-print(test2.convert_to_gregorian())
-print(test3.convert_to_gregorian())
-print("\nMasehi ke Hijriah")
-print(test4.convert_to_hijriah())
-print(test5.convert_to_hijriah())
-print(test6.convert_to_hijriah())
-# print(test6.hitung_variabel())
+	def test_hijri_1(self):
+		year = 1421
+		month = 1
+		day = 1
+		expected = "2000/4/6"
+		test = HijriahKeMasehi(year, month, day)
+		self.assertEqual(test.convert_to_gregorian(), expected)
+
+	def test_hijri_2(self):
+		year = 1444
+		month = 10
+		day = 12
+		expected = "2023/5/3"
+		test = HijriahKeMasehi(year, month, day)
+		self.assertEqual(test.convert_to_gregorian(), expected)
+
+	def test_hijri_3(self):
+		year = 1342
+		month = 2
+		day = 29
+		expected = "1923/10/11"
+		test = HijriahKeMasehi(year, month, day)
+		self.assertEqual(test.convert_to_gregorian(), expected)
+
+	def test_gregorian_1(self):
+		year = 1991
+		month = 8
+		day = 13
+		expected = "1412/2/2"
+		test = MasehiKeHijriah(year, month, day)
+		self.assertEqual(test.convert_to_hijriah(), expected)
+
+	def test_gregorian_2(self):
+		year = 2000
+		month = 28
+		day = 1
+		expected = "1423/1/22"
+		test = MasehiKeHijriah(year, month, day)
+		self.assertEqual(test.convert_to_hijriah(), expected)
+
+	def test_gregorian_3(self):
+		year = 2020
+		month = 1
+		day = 15
+		expected = "1441/5/19"
+		test = MasehiKeHijriah(year, month, day)
+		self.assertEqual(test.convert_to_hijriah(), expected)
+
+unittest.main()
